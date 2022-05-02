463a9d7fb02c37b003c2ee74497c5324971f0e2d
qiushi <qiushi.yann@gmail.com>
2022-05-02 15:44:08 -0500

    use alias for pyspark.sql.functions

--- jobs/actions.py
+++ jobs/actions.py
@@ -1,16 +1,25 @@
-from pyspark.sql import DataFrame
-from pyspark.sql.functions import *
 from datetime import date
 
+import pyspark.sql.functions as F
+from pyspark.sql import DataFrame
 
-def update_(df: DataFrame, user_id: int, name: str, city: str, last_login: date) -> DataFrame:
-    return df.withColumn(
-        "name", when(col("user_id") == user_id, name).otherwise(col("name"))
-    ).withColumn(
-        "city", when(col("user_id") == user_id, city).otherwise(col("city"))
-    ).withColumn(
-        "last_login", when(col("user_id") == user_id,
-                           last_login).otherwise(col("last_login"))
+
+def update_(
+    df: DataFrame, user_id: int, name: str, city: str, last_login: date
+) -> DataFrame:
+    return (
+        df.withColumn(
+            "name", F.when(F.col("user_id") == user_id, name).otherwise(F.col("name"))
+        )
+        .withColumn(
+            "city", F.when(F.col("user_id") == user_id, city).otherwise(F.col("city"))
+        )
+        .withColumn(
+            "last_login",
+            F.when(F.col("user_id") == user_id, last_login).otherwise(
+                F.col("last_login")
+            ),
+        )
     )
 
 
@@ -22,5 +31,6 @@ def insert_(df: DataFrame, **kwargs) -> DataFrame:
     spark = kwargs.get("spark")
     del kwargs["spark"]
     new_row = spark.createDataFrame(
-        [kwargs], schema="`user_id` INT, `name` STRING, `city` STRING, last_login DATE")
+        [kwargs], schema="`user_id` INT, `name` STRING, `city` STRING, last_login DATE"
+    )
     return df.union(new_row)
--- jobs/local_job.py
+++ jobs/local_job.py
@@ -1,27 +1,30 @@
-from typing import Union, List
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import dense_rank, col, when
-from pyspark.sql.window import Window
-from pathlib import Path
-from functools import reduce
 import os
-from jobs.downloader import Downloader
+from functools import reduce
+from pathlib import Path
+from typing import List, Union
+
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import col, dense_rank, when
+from pyspark.sql.window import Window
+
 from jobs.actions import delete_, insert_, update_
-from jobs.utils import city_to_state, find_src_with_largest_version, get_login_rate, count_distinct
+from jobs.downloader import Downloader
+from jobs.utils import (
+    city_to_state,
+    count_distinct,
+    find_src_with_largest_version,
+    get_login_rate,
+)
 
 
 class Job:
     def __init__(self):
         self.data_dir = Path().resolve() / "data"
         self.spark = SparkSession.builder.appName("cdc-pyspark").getOrCreate()
-        self.actions = {
-            "U": update_,
-            "I": insert_,
-            "D": delete_
-        }
+        self.actions = {"U": update_, "I": insert_, "D": delete_}
         self.schema = {
             "main": "`_c0` INT, `_c1` STRING, `_c2` STRING, `_c3` DATE",
-            "cdc": "`_c0` STRING, `_c1` INT, `_c2` STRING, `_c3` STRING, `_c4` DATE"
+            "cdc": "`_c0` STRING, `_c1` INT, `_c2` STRING, `_c3` STRING, `_c4` DATE",
         }
 
     def get_src(self, type: str) -> Union[str, List[str]]:
@@ -41,20 +44,20 @@ class Job:
 
     def rename_cols(self, df: DataFrame, type) -> DataFrame:
         if type == "main":
-            df_renamed = (df
-                          .withColumnRenamed("_c0", "user_id")
-                          .withColumnRenamed("_c1", "name")
-                          .withColumnRenamed("_c2", "city")
-                          .withColumnRenamed("_c3", "last_login")
-                          )
+            df_renamed = (
+                df.withColumnRenamed("_c0", "user_id")
+                .withColumnRenamed("_c1", "name")
+                .withColumnRenamed("_c2", "city")
+                .withColumnRenamed("_c3", "last_login")
+            )
         elif type == "cdc":
-            df_renamed = (df
-                          .withColumnRenamed("_c0", "action")
-                          .withColumnRenamed("_c1", "user_id")
-                          .withColumnRenamed("_c2", "name")
-                          .withColumnRenamed("_c3", "city")
-                          .withColumnRenamed("_c4", "last_login")
-                          )
+            df_renamed = (
+                df.withColumnRenamed("_c0", "action")
+                .withColumnRenamed("_c1", "user_id")
+                .withColumnRenamed("_c2", "name")
+                .withColumnRenamed("_c3", "city")
+                .withColumnRenamed("_c4", "last_login")
+            )
         return df_renamed
 
     def read_df(self, src: str, type: str) -> DataFrame:
@@ -72,7 +75,7 @@ class Job:
         if type == "cdc":
             data = reduce(
                 lambda x, y: x.union(y),
-                [self.read_df(src, type=type) for src in src_list]
+                [self.read_df(src, type=type) for src in src_list],
             )
         elif type == "main":
             src = find_src_with_largest_version(src_list)
@@ -88,41 +91,45 @@ class Job:
             if action == "D":
                 main_data = processor(main_data, user_id=row["user_id"])
             elif action == "I":
-                main_data = processor(main_data,
-                                      spark=self.spark,
-                                      user_id=row["user_id"],
-                                      name=row["name"],
-                                      city=row["city"],
-                                      last_login=row["last_login"])
+                main_data = processor(
+                    main_data,
+                    spark=self.spark,
+                    user_id=row["user_id"],
+                    name=row["name"],
+                    city=row["city"],
+                    last_login=row["last_login"],
+                )
             elif action == "U":
-                main_data = processor(main_data,
-                                      user_id=row["user_id"],
-                                      name=row["name"],
-                                      city=row["city"],
-                                      last_login=row["last_login"])
+                main_data = processor(
+                    main_data,
+                    user_id=row["user_id"],
+                    name=row["name"],
+                    city=row["city"],
+                    last_login=row["last_login"],
+                )
         return main_data
 
     def prepare_main_data(self, main_data: DataFrame) -> DataFrame:
         city_window = Window().partitionBy("city")
-        prepared_data = (main_data
-                         .withColumn(
-                             "city", when(col("city") == "Nashville-Davidson",
-                                          "Nashville").otherwise(col("city"))
-                         )
-                         .withColumn(
-                             "state", city_to_state(col("city"))
-                         )
-                         .withColumn(
-                             "last_login_rank_by_city", dense_rank().over(
-                                 city_window.orderBy(col("last_login").desc()))
-                         ).withColumn(
-                             "login_rate_last_3_months_by_city", get_login_rate(
-                                 col("last_login")).over(city_window)
-                         ).withColumn(
-                             "users_by_city", count_distinct(
-                                 col("user_id")).over(city_window)
-                         ).orderBy(col("city"), col("last_login_rank_by_city").desc())
-                         )
+        prepared_data = (
+            main_data.withColumn(
+                "city",
+                when(col("city") == "Nashville-Davidson", "Nashville").otherwise(
+                    col("city")
+                ),
+            )
+            .withColumn("state", city_to_state(col("city")))
+            .withColumn(
+                "last_login_rank_by_city",
+                dense_rank().over(city_window.orderBy(col("last_login").desc())),
+            )
+            .withColumn(
+                "login_rate_last_3_months_by_city",
+                get_login_rate(col("last_login")).over(city_window),
+            )
+            .withColumn("users_by_city", count_distinct(col("user_id")).over(city_window))
+            .orderBy(col("city"), col("last_login_rank_by_city").desc())
+        )
         return prepared_data
 
 
